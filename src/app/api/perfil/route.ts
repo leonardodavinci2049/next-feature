0e85cba0c7f6a424e4fab4f24edc9e9adacdc816
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2025-06-09 08:36:57 -0300

    feat: adicionar suporte à rota POST para atualização de perfil com busca de parâmetros

--- src/app/api/perfil/route.ts
+++ src/app/api/perfil/route.ts
@@ -1,4 +1,5 @@
 import { cookies } from "next/headers";
+import { NextRequest } from "next/server";
 
 const API_URL = process.env.API_URL;
 
@@ -33,6 +34,29 @@ export async function GET() {
   } catch (error) {
     console.error("Erro ao buscar cursos:", error);
     return new Response(JSON.stringify({ error: "Erro ao buscar cursos" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
+
+
+export async function POST(request: NextRequest) {
+  try {
+    
+  const param = await request.nextUrl.searchParams;
+  const vlParamBusca = param.get("busca");
+
+
+  return new Response(JSON.stringify({"busca": vlParamBusca }), {
+    headers: { "Content-Type": "application/json" },
+  });
+
+
+
+  } catch (error) {
+    console.error("Erro ao atualizar perfil:", error);
+    return new Response(JSON.stringify({ error: "Erro ao atualizar perfil" }), {
       status: 500,
       headers: { "Content-Type": "application/json" },
     });
